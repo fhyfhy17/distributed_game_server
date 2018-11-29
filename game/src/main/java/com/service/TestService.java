bbe5be740446a1802cf5c2b0cc6f8e4de9a45914
fenghaiyu <fenghaiyu@fotoable.com>
2018-11-29 20:31:34 +0800

    啊哈，研究过ehcache，但这次仿佛找到了思路啊，write behind走起。

--- game/src/main/java/com/service/TestService.java
+++ game/src/main/java/com/service/TestService.java
@@ -8,14 +8,18 @@ import com.entry.UserEntry;
 import com.event.playerEvent.TestEvent;
 import com.google.common.eventbus.Subscribe;
 import lombok.extern.slf4j.Slf4j;
+import org.ehcache.Cache;
+import org.ehcache.CacheManager;
+import org.ehcache.config.builders.CacheConfigurationBuilder;
+import org.ehcache.config.builders.CacheManagerBuilder;
+import org.ehcache.config.builders.ResourcePoolsBuilder;
+import org.ehcache.config.units.MemoryUnit;
+import org.ehcache.impl.internal.loaderwriter.writebehind.WriteBehind;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.cache.caffeine.CaffeineCache;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Map;
 
 @Service
 @EventListener
@@ -33,17 +37,17 @@ public class TestService {
 
     @Subscribe
     public void test(TestEvent testEvent) {
-        log.info("test = {}", testEvent.getTestWord());
-        for (String cacheName : cachemanager.getCacheNames()) {
-            System.out.println("cacheName: " + cacheName);
-        }
-
-        CaffeineCache playerEntryCache = (CaffeineCache) cachemanager.getCache("PlayerEntryCache");
-        for (Map.Entry<Object, Object> entry : playerEntryCache.getNativeCache().asMap().entrySet()) {
-            System.out.println(entry.getKey());
-            System.out.println(entry.getValue());
-            System.out.println("=====================");
-        }
+//        log.info("test = {}", testEvent.getTestWord());
+//        for (String cacheName : cachemanager.getCacheNames()) {
+//            System.out.println("cacheName: " + cacheName);
+//        }
+//
+//        CaffeineCache playerEntryCache = (CaffeineCache) cachemanager.getCache("PlayerEntryCache");
+//        for (Map.Entry<Object, Object> entry : playerEntryCache.getNativeCache().asMap().entrySet()) {
+//            System.out.println(entry.getKey());
+//            System.out.println(entry.getValue());
+//            System.out.println("=====================");
+//        }
     }
 
     @Cacheable(value = "PlayerEntryCache", sync = true, key = "'playerEntryCache.'.concat(#name)")
@@ -60,5 +64,47 @@ public class TestService {
     public void insertPlayer(PlayerEntry playerEntry) {
         playerRepository.save(playerEntry);
     }
+
+    public void aaa() {
+
+
+        CacheManager cacheManager = CacheManagerBuilder.newCacheManagerBuilder().build();
+        Cache<String, String> myCache = cacheManager.createCache("myCache",
+                CacheConfigurationBuilder.newCacheConfigurationBuilder(String.class, String.class,
+                        ResourcePoolsBuilder.newResourcePoolsBuilder().heap(100, MemoryUnit.MB))
+                        .withDispatcherConcurrency(4)
+                        .withLoaderWriter(new WriteBehind<String, String>() {
+                            @Override
+                            public void start() {
+
+                            }
+
+                            @Override
+                            public void stop() {
+
+                            }
+
+                            @Override
+                            public long getQueueSize() {
+                                return 0;
+                            }
+
+                            @Override
+                            public String load(String s) throws Exception {
+                                return null;
+                            }
+
+                            @Override
+                            public void write(String s, String s2) throws Exception {
+
+                            }
+
+                            @Override
+                            public void delete(String s) throws Exception {
+
+                            }
+                        }));
+    }
+
 
 }
